e39bca76d1f4804ad0282be7d960e719cd9ed08a
Igor <efimov.036@gmail.com>
2020-06-19 20:19:03 +0700

    configured swiper, added button

--- src/scripts/blocks/news/news__swiper.js
+++ src/scripts/blocks/news/news__swiper.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
-    loop: true
+    slidesPerView:4,
+    spaceBetween:25,
+    loop: true,
+    breakpoints:{
+      992:{
+        slidesPerView:4,
+      },
+      768:{
+        slidesPerView: 2,
+      },
+      320:{
+        slidesPerView:1,
+        slidesOffsetAfter:50,
+      },
+    },
+    navigation:{
+      nextEl:".button-next",
+    },
   })
 });
